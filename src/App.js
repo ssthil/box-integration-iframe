725c495cd32188cf9d9ba71781d4a7273aa59998
Senthil Sivaramakrishnanan <senthil.sivaramakrishnan.ext@proximus.com@Senthils-MacBook-Pro-Px.local>
2020-06-08 18:04:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav className="header">
+          <ul>
+            <li>
+              <Link to="/">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+        <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/page2" exact>
+            <Page2 />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <div className="wrapper">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quasi repudiandae, tempora fugiat quas sequi
+            dignissimos impedit necessitatibus quaerat veritatis alias accusamus similique autem deserunt eligendi harum est
+            quos quae!</p>
+        <h2>Page 1 - iframe - single note</h2>
+    
+        <p>Narrow screen devices (e.g. mobiles) render pages in a virtual window or viewport, which is usually wider than
+            the screen, and then shrink the rendered result down so it can all be seen at once. Users can then pan and zoom
+            to see different areas of the page. For example, if a mobile screen has a width of 640px, pages might be
+            rendered with a virtual viewport of 980px, and then it will be shrunk down to fit into the 640px space.</p>
+    
+            <br/>
+            <div style={{textAlign: "center"}}>
+
+            <iframe title="box" src="https://app.box.com/embed/s/ux61e9fl5nv887e9xdsxp8aop5rcbyeg?sortColumn=date&view=list" width="98%" height="600" frameBorder="0" allowFullScreen />
+            </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Page2() {
+  return(
+    <div>
+      <div className="wrapper">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae quasi repudiandae, tempora fugiat quas sequi
+            dignissimos impedit necessitatibus quaerat veritatis alias accusamus similique autem deserunt eligendi harum est
+            quos quae!</p>
+        <h2>Page 2 - iframe - different folder</h2>
+    
+        <p>Narrow screen devices (e.g. mobiles) render pages in a virtual window or viewport, which is usually wider than
+            the screen, and then shrink the rendered result down so it can all be seen at once. Users can then pan and zoom
+            to see different areas of the page. For example, if a mobile screen has a width of 640px, pages might be
+            rendered with a virtual viewport of 980px, and then it will be shrunk down to fit into the 640px space.</p>
+    
+            <br/>
+            <div style={{textAlign: "center"}}>
+
+            <iframe title="box" src="https://app.box.com/embed/s/ezhi1pxzknox7z88rat2r2t0n2dqcznc?sortColumn=date&view=list" width="98%" height="600" frameBorder="0" allowFullScreen />
+            </div>
+      </div>
+      </div>
+  )
+}

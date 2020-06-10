03dfbbe72fc6ef57ddc5545f8907ff4e5cedafc4
Senthil Sivaramakrishnanan <senthil.sivaramakrishnan.ext@proximus.com@Senthils-MacBook-Pro-Px.local>
2020-06-10 13:05:08 +0200

    iframe url changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 
 import './App.css';
@@ -14,21 +13,21 @@ export default function App() {
       <div>
         <nav className="header">
           <ul>
-            <li>
+           {/*  <li>
               <Link to="/">Page 1</Link>
-            </li>
-            <li>
+            </li> */}
+           {/*  <li>
               <Link to="/page2">Page 2</Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
         <Switch>
         <Route path="/" exact>
             <Home />
           </Route>
-          <Route path="/page2" exact>
+          {/* <Route path="/page2" exact>
             <Page2 />
-          </Route>
+          </Route> */}
         </Switch>
       </div>
     </Router>
@@ -36,27 +35,40 @@ export default function App() {
 }
 
 function Home() {
+  // const [userName, setUserName] = useState('')
   return (
     <div>
       <div className="wrapper">
-        <h2>Page 1 - iframe - single note</h2>
+        <h2>RFI and RFP - iframe </h2>
     
         <p>Narrow screen devices (e.g. mobiles) render pages in a virtual window or viewport, which is usually wider than
             the screen, and then shrink the rendered result down so it can all be seen at once. Users can then pan and zoom
             to see different areas of the page. For example, if a mobile screen has a width of 640px, pages might be
             rendered with a virtual viewport of 980px, and then it will be shrunk down to fit into the 640px space.</p>
     
-            <br/>
+    {/* <form>
+
+      <div className="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required />
+
+        <button type="submit">Login</button>
+      </div>
+    </form> */}
+    
             <div style={{textAlign: "center"}}>
 
-            <iframe title="box" src="https://app.box.com/embed/s/ux61e9fl5nv887e9xdsxp8aop5rcbyeg?sortColumn=date&view=list" width="70%" height="600" frameBorder="0" allowFullScreen />
+            <iframe title="box" src="https://app.box.com/embed/folder/115212938312?sortColumn=date&view=list" width="70%" height="600" frameBorder="0" allowFullScreen />
             </div>
       </div>
     </div>
   );
 }
 
-function Page2() {
+/* function Page2() {
   return(
     <div>
       <div className="wrapper">
@@ -75,4 +87,4 @@ function Page2() {
       </div>
       </div>
   )
-}
+} */
